28f4301d4c256c2524910b5b423de04fe9d31887
gudetama <daniellewwise@gmail.com>
2023-07-12 00:00:42 -0400

    add page nav links

--- src/app/components/navBar/navBar.tsx
+++ src/app/components/navBar/navBar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./style.scss"
 import ToggleButton from "../toggleButton/toggleButton";
+import Link from "next/link";
 
 export default function NavBar({ toggleTheme }) {
 	return (
 		<nav className="navbar-container">
 			<ul className="nav navbar">
-				<li className="nav-item"><a href="#home">HOME</a></li>
-				<li className="nav-item"><a href="#projects">PROJECTS</a></li>
-				<li className="nav-item"><a href="#resume">RESUME</a></li>
+				<li className="nav-item"><Link href="/">HOME</Link></li>
+				<li className="nav-item"><Link href="/projects">PROJECTS</Link></li>
+				<li className="nav-item"><Link href="/resume">RESUME</Link></li>
 			</ul>
 			<ToggleButton toggleTheme={toggleTheme} />
 		</nav>
