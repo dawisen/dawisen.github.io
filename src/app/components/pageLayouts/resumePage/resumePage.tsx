0af04531b708ce818428501e9f89bc99a494fcbc
gudetama <daniellewwise@gmail.com>
2023-07-16 01:20:54 -0400

    add more content and styling for resume page

--- src/app/components/pageLayouts/resumePage/resumePage.tsx
+++ src/app/components/pageLayouts/resumePage/resumePage.tsx
@@ -6,9 +6,12 @@ export default function ResumePage() {
 	return (
     <div id="resume-container">
       <section id="contact-info">
-        <h1>Danielle Wisen</h1>
+        <div id="name-and-title">
+          <h1>Danielle Wisen</h1>
+          <p>Software Engineer</p>
+        </div>
         <div className="contact-links">
-          <Link href={'daniellewwise@gmail.com'}>email</Link>
+          <Link href={'daniellewwise@gmail.com'}>daniellewwise@gmail.com</Link>
           <Link href={'https://github.com/dawisen'}>GitHub</Link>
           <Link href={'https://www.linkedin.com/in/daniellewisen1/'}>LinkedIn</Link>
         </div>
@@ -23,15 +26,21 @@ export default function ResumePage() {
       </section>
       <section id="experience">
         <h2>Experience</h2>
-        <ul>
-          <li>WillowTree</li>
-          <li>JAG Global Learning</li>
-          <li>Japan Exchange and Teaching Program</li>
-        </ul>
+        <div>
+          <h3>WillowTree</h3>
+        </div>
+        <div>
+          <h3>JAG Global Learning</h3>
+        </div>
+        <div>
+          <h3>Japan Exchange and Teaching Program</h3>
+        </div>
       </section>
       <section id="skills"><h2>Skills</h2>
         <h3>Technical</h3>
+        <p>JavaScript, React, TypeScript, Express, Java, Node, Electron, MongoDB, JSON, Python Vue.js, MySQL</p>
         <h3>Tooling</h3>
+        <p>Heroku, Bitbucket, Jira, Git, AWS, GitHub Actions</p>
       </section>
       <section id="education"><h2>Education</h2>
         <ul>
