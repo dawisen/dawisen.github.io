87fcc25f2d189d7b33305f69547cad9f9606f64d
gudetama <daniellewwise@gmail.com>
2023-07-18 23:58:34 -0400

    added more text content and styling to resume page

--- src/app/components/pageLayouts/resumePage/resumePage.tsx
+++ src/app/components/pageLayouts/resumePage/resumePage.tsx
@@ -10,7 +10,7 @@ export default function ResumePage() {
           <h1>Danielle Wisen</h1>
           <p>Software Engineer</p>
         </div>
-        <div className="contact-links">
+        <div id="contact-links">
           <Link href={'daniellewwise@gmail.com'}>daniellewwise@gmail.com</Link>
           <Link href={'https://github.com/dawisen'}>GitHub</Link>
           <Link href={'https://www.linkedin.com/in/daniellewisen1/'}>LinkedIn</Link>
@@ -19,34 +19,81 @@ export default function ResumePage() {
       <section id="summary">
         <h2>Summary</h2>
         <p>
-          Software Engineer with writing and teaching background seeking new technical opportunities.
-          Two years of experience in JavaScript, React and five years of experience in writing and editing.
-          Committed learner and critical thinker.
+          Highly motivated engineer with a background in teaching and writing.
+          Experienced in both frontend and backend development with two years of experience
+          in JavaScript, React and five years of experience in writing and editing.
+          Committed learner and critical thinker seeking opportunities to build meaningful projects
+          with a passionate team.
         </p>
       </section>
       <section id="experience">
         <h2>Experience</h2>
         <div>
-          <h3>WillowTree</h3>
+          <p className="subsection-title">
+            Software Engineer
+            <a href="https://www.willowtreeapps.com/">WillowTree</a>
+          </p>
+          <ul>
+            <li>Developed and maintained a back-end API using Python, Amazon Web Services including Lambda, API Gateway, S3.</li>
+            <li>Leveraged AWS services such as Route 53, Cloudfront, and S3 for serving a front-end React application.</li>
+            <li>
+              Implemented GitHub Actions to automate the deployment process for production, staging, and development
+              environments, enabling linting and unit testing on each pull request and merge.
+            </li>
+            <li>
+              Contributed as a key member of the frontend development team, utilizing React and Next.js to recreate the client’s user
+              web portal, enhancing user experience and functionality.
+            </li>
+            <li>
+              Integrated analytics into the project, enabling comprehensive tracking and analysis of user interactions to create
+              actionable insights for the client that inform decision-making and future improvements with Mixpanel.
+            </li>
+            <li>
+              Actively participated in code reviews and provided valuable feedback to team members, ensuring adherence to best
+              practices, code quality, and efficient development processes.
+            </li>
+          </ul>
         </div>
         <div>
-          <h3>JAG Global Learning</h3>
+          <p className="subsection-title">
+            Software Engineer
+            <a href="https://jaggloballearning.com/">JAG Global Learning</a>
+          </p>
+          <ul>
+            <li>Started a new project using Electron and React to streamline the process of writing and designing on-boarding documentation
+              for Medicare Employees.
+              <ul>
+                <li>
+                  Developed client project in accordance to Section 508 federal guidelines to ensure that they are readily accessible to
+                  people with disabilities.
+                </li>
+              </ul>
+            </li>
+            <li>Translated product requirements from clients into technical specifications.</li>
+            <li>Actively communicated progress with stakeholders on a bi-weekly basis.</li>
+          </ul>
         </div>
         <div>
-          <h3>Japan Exchange and Teaching Program</h3>
+          <p className="subsection-title">
+            Assistant Language Teacher
+            <a href="https://jetprogramusa.org/">Japan Exchange and Teaching Program</a>
+            
+          </p>
+          <ul>
+            <li>Served as a cultural ambassador between the United States and Japan promoting English education.</li>
+            <li>Co-led an educational workshop for teachers on different learning methods of students.</li>
+          </ul>
         </div>
       </section>
       <section id="skills"><h2>Skills</h2>
-        <h3>Technical</h3>
+        <p className="subsection-title">Software</p>
         <p>JavaScript, React, TypeScript, Express, Java, Node, Electron, MongoDB, JSON, Python Vue.js, MySQL</p>
-        <h3>Tooling</h3>
+        <p className="subsection-title">Tooling</p>
         <p>Heroku, Bitbucket, Jira, Git, AWS, GitHub Actions</p>
       </section>
       <section id="education"><h2>Education</h2>
-        <ul>
-          <li>Full-Stack Coding Bootcamp Certificate, The University of North Carolina at Chapel Hill</li>
-          <li>Bachelor of Arts in English Language and Writing, North Carolina State University</li>
-        </ul>
+        <p className="subsection-title">Full-Stack Coding Bootcamp Certificate, The University of North Carolina at Chapel Hill</p>
+        <p className="subsection-title">Bachelor of Arts in English Language and Writing, North Carolina State University</p>
       </section>
     </div>
 	)
